0d01c85e1876c851ddfd0eb36b4f34cd1e020c22
github username <github email address>
2019-03-21 14:53:21 -0400

    adding navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'recipes#index'
+
+  # devise_for :users, controllers: { sessions: 'users/sessions' }
+
+
+  devise_scope :user do
+    get 'signup', to: 'users/registrations#new'
+  end
+
+  devise_scope :user do
+    get 'login', to: 'users/sessions#new'
+  end
+
+
+
+  
+  devise_for :users
 end
