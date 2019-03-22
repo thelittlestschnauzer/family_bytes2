2c0998aa34021030de99d70fd0e710245afc4b3e
github username <github email address>
2019-03-21 21:40:25 -0400

    creating recipes, associated to user and category

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,45 @@
 class RecipesController < ApplicationController
 
   def index 
+    if params[:category_id]
+      @category = Category.find_by(id: params[:category_id])
+      if @category.nil?
+        redirect_to root_path, "Category not found"
+      else
+        @recipes = @category.recipes 
+      end 
+    else 
+      @recipes = Recipe.all 
+    end 
   end 
 
   def new 
+    @recipe = current_user.recipes.build
+  end 
+
+  def create 
+    @recipe = category.recipes.build(recipe_params)
+    @recipe.image.attach(params[:recipe][:image])
+
+    if @recipe.save 
+      redirect_to category_recipe_path(@recipe.category_id, @recipe)
+    else 
+      render :new 
+    end  
+  end 
+
+  def show 
+    @recipe = Recipe.find(params[:id])
   end 
 
   private 
 
+  def set_recipe 
+    @recipe = Recipe.find(params[:id])
+  end 
+
   def recipe_params 
-    params.require(:recipe).permit(:title, :description, :image, :level, :yields)
+    params.require(:recipe).permit(:title, :description, :image, :level, :yields, :category_id)
   end 
   
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,16 @@
 class Recipe < ApplicationRecord
+  # belongs_to :user
+  belongs_to :category
+
   has_one_attached :image
+
+
+  def thumbnail 
+    return self.image.variant(resize: '250x250')
+  end 
+
+  def large
+    return self.image.variant(resize: '500x400') 
+  end 
+  
 end
